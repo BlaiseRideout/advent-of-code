0d543cbd07bd28c0951d8dc5bf3284c426912bc9
Blaise <blaise@blaise.in>
2022-12-03 21:45:53 -0800

    Cleaner solution inspired by zaquestion's

--- day4/day4.rs
+++ day4/day4.rs
@@ -1,32 +1,32 @@
 use std::io;
+use std::collections::HashSet;
 
-fn parse_pairs(lines: &Vec<String>) -> Vec::<Vec::<Vec::<i32>>> {
-    lines.iter().map(|line| -> Vec<Vec::<i32>> {
+fn parse_pairs(lines: &Vec<String>) -> Vec<Vec::<HashSet::<i32>>> {
+    lines.iter().map(|line| -> Vec<HashSet::<i32>> {
         line.split(",")
             .map(|range| range.split("-")
                  .map(|section| section.parse::<i32>()
                                   .expect("Couldn't parse int"))
                  .collect()
-             ).collect()
+             )
+             .map(|section: Vec::<_>|
+                      (section[0]..=section[1]).collect::<HashSet::<_>>())
+             .collect()
     }).collect()
 }
 
 fn part1(lines: &Vec<String>) -> usize {
     parse_pairs(lines).iter()
-        .filter(|line| -> bool {
-            (&line[0][0] >= &line[1][0] && &line[0][1] <= &line[1][1]) ||
-            (&line[1][0] >= &line[0][0] && &line[1][1] <= &line[0][1])
-        }).count()
+        .filter(|line|
+            line[0].is_superset(&line[1]) || line[1].is_superset(&line[0])
+        ).count()
 }
 
 fn part2(lines: &Vec<String>) -> usize {
     parse_pairs(lines).iter()
-        .filter(|line| -> bool {
-            (&line[0][0] >= &line[1][0] && &line[0][0] <= &line[1][1]) ||
-            (&line[0][1] >= &line[1][0] && &line[0][1] <= &line[1][1]) ||
-            (&line[1][0] >= &line[0][0] && &line[1][0] <= &line[0][1]) ||
-            (&line[1][1] >= &line[0][0] && &line[1][1] <= &line[0][1])
-        }).count()
+        .filter(|line|
+            !line[0].is_disjoint(&line[1]) && !line[1].is_disjoint(&line[0])
+        ).count()
 }
 
 fn main() {
