6b186809699b39799d90f047919ce8f0d51c1b7b
Blaise <blaise@blaise.in>
2022-12-14 12:05:35 -0800

    Improved performance and visualization

--- day14/day14.rs
+++ day14/day14.rs
@@ -1,22 +1,25 @@
 use itertools::Itertools;
-use once_cell::sync::Lazy;
-use std::cell::Cell;
 use std::cmp::{max, min};
 use std::collections::HashSet;
 use std::io;
-use std::ops::RangeInclusive;
 
 type Point = (isize, isize);
-type Rock = (RangeInclusive<isize>, RangeInclusive<isize>);
 
 #[derive(PartialEq, Eq, Clone)]
-struct Grid {
-    rocks: Vec<Rock>,
+struct Bounds {
     minx: isize,
     maxx: isize,
     miny: isize,
     maxy: isize,
+}
+
+#[derive(PartialEq, Eq, Clone)]
+struct Grid {
+    rocks: HashSet<Point>,
+    rock_bounds: Bounds,
     sands: HashSet<Point>,
+    sand_bounds: Bounds,
+    pt2: bool,
 }
 
 #[derive(PartialEq, Eq, Hash, Clone)]
@@ -32,10 +35,12 @@ fn add(p1: Point, p2: Point) -> Point {
 
 impl Grid {
     fn parse(lines: &Vec<String>) -> Grid {
+        let merge_hashsets =
+            |hs1: HashSet<Point>, hs2: HashSet<Point>| hs1.union(&hs2).cloned().collect();
         Self::new(
             lines
                 .into_iter()
-                .map(|line| -> Vec<Rock> {
+                .filter_map(|line| {
                     line.split("->")
                         .map(|point| -> Point {
                             let mut pointstrs = point.trim().split(",");
@@ -53,60 +58,62 @@ impl Grid {
                             )
                         })
                         .tuple_windows::<(_, _)>()
-                        .map(|rock| {
-                            (
-                                min(rock.0 .0, rock.1 .0)..=max(rock.0 .0, rock.1 .0),
-                                min(rock.0 .1, rock.1 .1)..=max(rock.0 .1, rock.1 .1),
-                            )
+                        .filter_map(|rock| {
+                            (min(rock.0 .0, rock.1 .0)..=max(rock.0 .0, rock.1 .0))
+                                .map(|x| {
+                                    (min(rock.0 .1, rock.1 .1)..=max(rock.0 .1, rock.1 .1))
+                                        .map(|y| (x, y))
+                                        .collect::<HashSet<Point>>()
+                                })
+                                .reduce(merge_hashsets)
                         })
-                        .collect()
+                        .reduce(merge_hashsets)
                 })
-                .reduce(|mut rs1, mut rs2| {
-                    rs1.append(&mut rs2);
-                    rs1
-                })
+                .reduce(merge_hashsets)
                 .expect("Couldn't get rocks"),
         )
     }
-    fn new(rocks: Vec<Rock>) -> Grid {
-        Grid {
-            minx: *rocks
+
+    fn new(rocks: HashSet<Point>) -> Grid {
+        let rock_bounds = Bounds {
+            minx: rocks
                 .iter()
-                .map(|rock| rock.0.start())
+                .map(|rock| rock.0)
                 .min()
                 .expect("Couldn't get minx"),
-            maxx: *rocks
+            maxx: rocks
                 .iter()
-                .map(|rock| rock.0.end())
+                .map(|rock| rock.0)
                 .max()
                 .expect("Couldn't get maxx"),
-            miny: *rocks
+            miny: rocks
                 .iter()
-                .map(|rock| rock.1.start())
+                .map(|rock| rock.1)
                 .min()
                 .expect("Couldn't get miny"),
-            maxy: *rocks
+            maxy: rocks
                 .iter()
-                .map(|rock| rock.1.end())
+                .map(|rock| rock.1)
                 .max()
                 .expect("Couldn't get maxy"),
+        };
+        Grid {
+            sand_bounds: rock_bounds.clone(),
+            rock_bounds,
             rocks,
             sands: [].into_iter().collect(),
+            pt2: false,
         }
     }
 
-    fn add_ground(&mut self) {
-        self.maxy += 2;
-        self.rocks
-            .push((isize::MIN..=isize::MAX, self.maxy..=self.maxy));
+    fn set_pt2(&mut self, pt2: bool) {
+        self.pt2 = pt2;
     }
 
     fn position_occupied_by(&self, p: Point) -> Occupation {
-        if self
-            .rocks
-            .iter()
-            .any(|rock| rock.0.contains(&p.0) && rock.1.contains(&p.1))
-        {
+        if self.pt2 && p.1 >= self.rock_bounds.maxy + 2 {
+            Occupation::Rock
+        } else if self.rocks.contains(&p) {
             Occupation::Rock
         } else if self.sands.contains(&p) {
             Occupation::Sand
@@ -117,17 +124,18 @@ impl Grid {
 
     fn drop_sand(&mut self, start_pos: Point) -> Option<Point> {
         let mut sand_pos: Point = start_pos;
-        while sand_pos.1 <= self.maxy {
+        while sand_pos.1 <= self.rock_bounds.maxy + 2 {
             if let Some(dest) = [(0, 1), (-1, 1), (1, 1)]
                 .into_iter()
-                .map(|p| {
-                    let p = add(sand_pos, p);
-                    (p, self.position_occupied_by(p))
-                })
-                .find(|(_, occupation)| *occupation == Occupation::Air)
+                .map(|p| add(sand_pos, p))
+                .find(|p| self.position_occupied_by(*p) == Occupation::Air)
             {
-                sand_pos = dest.0;
+                sand_pos = dest;
             } else {
+                self.sand_bounds.minx = min(self.sand_bounds.minx, sand_pos.0);
+                self.sand_bounds.maxx = max(self.sand_bounds.maxx, sand_pos.0);
+                self.sand_bounds.miny = min(self.sand_bounds.miny, sand_pos.1);
+                self.sand_bounds.maxy = max(self.sand_bounds.maxy, sand_pos.1);
                 self.sands.insert(sand_pos);
                 return Some(sand_pos);
             }
@@ -136,10 +144,32 @@ impl Grid {
     }
 
     fn print(&self) {
-        println!("{} .. {}", self.minx, self.maxx);
-        for y in min(self.miny, 0)..=self.maxy {
+        let minx = min(self.rock_bounds.minx, self.sand_bounds.minx);
+        let miny = min(self.rock_bounds.miny, self.sand_bounds.miny);
+        let maxx = max(self.rock_bounds.maxx, self.sand_bounds.maxx);
+        let maxy = max(self.rock_bounds.maxy, self.sand_bounds.maxy);
+        println!(
+            "{}",
+            (0..3)
+                .rev()
+                .map(|digit| {
+                    let power = isize::pow(10, digit);
+                    "  ".to_string()
+                        + &(minx..=maxx)
+                            .map(|x| {
+                                if x % 10 == 0 || x == maxx || x == minx {
+                                    ((x / power) % 10).to_string()
+                                } else {
+                                    " ".to_string()
+                                }
+                            })
+                            .join("")
+                })
+                .join("\n")
+        );
+        for y in min(miny, 0)..=maxy {
             print!("{} ", y);
-            for x in self.minx..=self.maxx {
+            for x in minx..=maxx {
                 print!(
                     "{}",
                     match self.position_occupied_by((x, y)) {
@@ -154,7 +184,7 @@ impl Grid {
     }
 }
 
-static DEBUG: bool = true;
+static DEBUG: bool = false;
 
 fn part1(mut grid: Grid) -> usize {
     if DEBUG {
@@ -171,7 +201,7 @@ fn part1(mut grid: Grid) -> usize {
 }
 
 fn part2(mut grid: Grid) -> usize {
-    grid.add_ground();
+    grid.set_pt2(true);
     if DEBUG {
         grid.print();
     }
