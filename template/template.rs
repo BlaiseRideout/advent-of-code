4f7971499d2a1a0b094cdd7d0c868f361c258767
Blaise <blaise@blaise.in>
2022-12-14 20:48:31 -0800

    Switch input to file read for debugging

--- template/template.rs
+++ template/template.rs
@@ -1,4 +1,6 @@
-use std::io;
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
 
 fn part1(lines: &Vec<String>) -> usize {
     lines.len()
@@ -9,8 +11,18 @@ fn part2(lines: &Vec<String>) -> usize {
 }
 
 fn main() {
-    let lines: Vec<String> = io::stdin().lines()
-                                .filter_map(Result::ok).collect();
+    if env::args().count() != 2 {
+        return println!(
+            "Usage: {} [path/to/input_file]",
+            env::args().next().expect("Couldn't get executable name")
+        );
+    }
+    let input_name: String = env::args().skip(1).next().expect("First argument");
+    let f = File::open(input_name).expect("Couldn't open input file");
+    let lines: Vec<String> = io::BufReader::new(f)
+        .lines()
+        .filter_map(Result::ok)
+        .collect();
 
     println!("Part 1: {}", part1(&lines));
     println!("Part 2: {}", part2(&lines));
