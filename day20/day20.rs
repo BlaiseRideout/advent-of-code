c0f22df5fde6170f6f9626f408bf8076aa3a65ab
Blaise <blaise@blaise.in>
2022-12-19 23:40:26 -0800

    Day 20 pt1 solution

--- day20/day20.rs
+++ day20/day20.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -11,86 +11,108 @@ fn parse(lines: &Vec<String>) -> Vec<isize> {
         .collect()
 }
 
-fn wrapped_mod(mut i: isize, len: usize) -> usize {
+fn simple_wrapped_mod(mut i: isize, len: usize) -> usize {
     while i < 0 {
         i += len as isize;
     }
     i as usize % len
 }
 
-fn part1(fields: &Vec<isize>) -> isize {
+fn find_wrapped_index(old_i: isize, mut offset: isize, len: usize) -> usize {
+    if offset < 0 {
+        offset += (len as isize - 1) * (-offset / (len as isize - 1) + 1);
+    }
+    if offset + old_i >= len as isize {
+        offset -= (len as isize - 1) * ((offset + old_i) / (len as isize - 1));
+    }
+    let ret = (old_i + offset) as usize;
+    assert!((0..len).contains(&ret));
+    ret
+}
+
+fn mix_numbers(fields: &Vec<isize>, indices: &Vec<usize>) -> (Vec<isize>, Vec<usize>) {
     let len = fields.len();
 
-    let ret = fields
+    let ret = indices
         .into_iter()
-        .enumerate()
         .fold(
             (
                 Vec::<(Range<usize>, isize)>::new(),
                 Vec::<(usize, isize)>::new(),
             ),
-            |(mut offsets, mut ret), (i, f)| {
+            |(mut offsets, mut ret), i| {
+                let f = fields[*i];
                 let wrapped_offset_i: usize =
-                    offsets.iter().fold(i, |offset_i, (range, range_offset)| {
+                    offsets.iter().fold(*i, |offset_i, (range, range_offset)| {
                         if range.contains(&offset_i) {
-                            dbg!(wrapped_mod(offset_i as isize + range_offset, len))
+                            simple_wrapped_mod(offset_i as isize + range_offset, len)
                         } else {
                             offset_i
                         }
                     });
-                let real_i: usize = wrapped_mod(
-                    wrapped_offset_i as isize + if *f < 0 { *f - 1 } else { *f },
-                    fields.len(),
-                );
+                let real_i: usize = find_wrapped_index(wrapped_offset_i as isize, f, len);
                 let removal_offset: (Range<usize>, isize) = (wrapped_offset_i + 1..len, -1);
 
-                /*
-                if *f < -(wrapped_offset_i as isize) && removal_offset.0.contains(&real_i) {
-                    real_i = (real_i as isize + removal_offset.1) as usize;
-                }
-                */
-
-                println!(
-                    "i: {} wrapped_offset_i: {} real_i: {}",
-                    i, wrapped_offset_i, real_i
-                );
-
-                let insertion_offset: (Range<usize>, isize) = (real_i + 1..len, 1);
+                let insertion_offset: (Range<usize>, isize) = (real_i..len, 1);
                 let new_offsets = [removal_offset, insertion_offset];
                 new_offsets.iter().for_each(|new_offset| {
                     ret.iter_mut().for_each(|mut field| {
                         if new_offset.0.contains(&field.0) {
-                            field.0 = wrapped_mod(field.0 as isize + new_offset.1, len);
+                            field.0 = simple_wrapped_mod(field.0 as isize + new_offset.1, len);
                         }
                     });
                 });
                 offsets.extend(new_offsets);
-                ret.push((real_i, *f));
-                ret.sort();
-                (offsets, dbg!(ret))
+                ret.push((real_i, f));
+                (offsets, ret)
             },
         )
         .1;
-    let ret = ret.iter().fold(vec![0; len], |mut ret, (i, f)| {
-        ret[*i] = *f;
-        ret
-    });
-    let zero_pos = ret
+    (
+        ret.iter().fold(vec![0; len], |mut ret, (i, f)| {
+            ret[*i] = *f;
+            ret
+        }),
+        ret.iter().map(|(i, _)| *i).collect(),
+    )
+}
+fn mix_numbers_simple(fields: &Vec<isize>) -> (Vec<isize>, Vec<usize>) {
+    mix_numbers(fields, &(0..fields.len()).collect())
+}
+
+fn grove_coordinates(mixed: &Vec<isize>) -> isize {
+    let zero_pos = mixed
         .iter()
         .position(|i| *i == 0)
         .expect("Couldn't find position of value 0");
-
     [1000, 2000, 3000]
         .into_iter()
-        .map(|i| wrapped_mod(zero_pos as isize + i, len))
-        .inspect(|i| println!("wrapped_i: {}", i))
-        .map(|i| ret[i])
-        .inspect(|i| println!("val: {}", i))
+        .map(|i| simple_wrapped_mod(zero_pos as isize + i, mixed.len()))
+        //.inspect(|i| println!("wrapped_i: {}", i))
+        .map(|i| mixed[i])
+        //.inspect(|i| println!("val: {}", i))
         .sum::<isize>()
 }
 
-fn part2(lines: &Vec<String>) -> usize {
-    lines.len()
+fn part1(fields: &Vec<isize>) -> isize {
+    let (mixed, _) = mix_numbers_simple(fields);
+    grove_coordinates(&mixed)
+}
+
+fn part2(fields: &Vec<isize>) -> isize {
+    static DECRYPTION_KEY: isize = 811589153;
+    static NUM_ITERATIONS: usize = 10;
+    let mut mixed = fields
+        .iter()
+        .map(|x| x * DECRYPTION_KEY)
+        .collect::<Vec<_>>();
+    println!("Initial arrangement: \n{:?}", mixed);
+    let mut indices: Vec<usize> = (0..fields.len()).collect();
+    for iteration in 1..=NUM_ITERATIONS {
+        (mixed, indices) = mix_numbers(&mixed, &indices);
+        println!("After {} round of mixing: \n{:?}", iteration, mixed);
+    }
+    grove_coordinates(&mixed)
 }
 
 fn main() {
@@ -110,5 +132,5 @@ fn main() {
     let nums = parse(&lines);
 
     println!("Part 1: {}", part1(&nums));
-    println!("Part 2: {}", part2(&lines));
+    //println!("Part 2: {}", part2(&nums));
 }
