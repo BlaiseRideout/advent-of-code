5e85c797fec7eb16534cf0f857193bf811c9de9d
Blaise <blaise@blaise.in>
2023-11-30 22:18:25 -0800

    Generate regex from map

--- 2023/day01/day01.rs
+++ 2023/day01/day01.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -6,66 +7,89 @@ use itertools::Itertools;
 use once_cell::sync::Lazy;
 use regex::Regex;
 
-fn digits_to_value(nums: Vec<char>) -> usize {
-    let digits = if nums.len() < 1 {
-        ['0', '0']
-    } else {
-        [*nums.first().unwrap(), *nums.last().unwrap()]
-    };
-    digits.iter().join("").parse::<usize>().unwrap()
+fn digits_to_value(num1: char, num2: char) -> usize {
+    [num1, num2]
+        .iter()
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap()
 }
 
 fn part1(lines: &Vec<String>) -> usize {
     lines
         .iter()
         .map(|line| {
-            let nums = line
+            let num1 = line.chars().find(|c| *c >= '0' && *c <= '9').unwrap_or('0');
+            let num2 = line
                 .chars()
-                .filter(|c| *c >= '0' && *c <= '9')
-                .collect::<Vec<_>>();
-            digits_to_value(nums)
+                .rfind(|c| *c >= '0' && *c <= '9')
+                .unwrap_or('0');
+            digits_to_value(num1, num2)
         })
         .sum()
 }
 
+static DIGIT_TO_STR: Lazy<HashMap<&str, u8>> = Lazy::new(|| {
+    let mut m = HashMap::new();
+    m.insert("one", 1);
+    m.insert("two", 2);
+    m.insert("three", 3);
+    m.insert("four", 4);
+    m.insert("five", 5);
+    m.insert("six", 6);
+    m.insert("seven", 7);
+    m.insert("eight", 8);
+    m.insert("nine", 9);
+    m.insert("zero", 0);
+    m
+});
+
 fn parse_digit(s: &str) -> char {
-    (match s {
-        "one" => 1,
-        "two" => 2,
-        "three" => 3,
-        "four" => 4,
-        "five" => 5,
-        "six" => 6,
-        "seven" => 7,
-        "eight" => 8,
-        "nine" => 9,
-        "zero" => 0,
-        m => m.parse::<u8>().expect("Couldn't parse number"),
+    (if let Some(v) = DIGIT_TO_STR.get(s) {
+        *v
+    } else {
+        s.parse::<u8>().expect("Couldn't parse number")
     } + '0' as u8) as char
 }
 
 fn part2(lines: &Vec<String>) -> usize {
     static FORWARD_NUM_RE: Lazy<Regex> = Lazy::new(|| {
-        Regex::new(r"[0-9]|one|two|three|four|five|six|seven|eight|nine|zero")
-            .expect("Couldn't parse regex")
+        Regex::new(
+            DIGIT_TO_STR
+                .keys()
+                .map(|k| *k)
+                .chain(["[0-9]"].into_iter())
+                .intersperse("|")
+                .collect::<String>()
+                .as_str(),
+        )
+        .expect("Couldn't parse regex")
     });
     static BACKWARD_NUM_RE: Lazy<Regex> = Lazy::new(|| {
-        Regex::new(r"[0-9]|orez|enin|thgie|neves|xis|evif|ruof|eerht|owt|eno")
-            .expect("Couldn't parse regex")
+        Regex::new(
+            DIGIT_TO_STR
+                .keys()
+                .map(|k| k.chars().rev().collect::<String>())
+                .chain([String::from("[0-9]")].into_iter())
+                .intersperse(String::from("|"))
+                .collect::<String>()
+                .as_str(),
+        )
+        .expect("Couldn't parse regex")
     });
 
     lines
         .iter()
         .map(|line| {
-            let num2 = BACKWARD_NUM_RE
-                .find(line.chars().rev().collect::<String>().as_str())
-                .map(|s| parse_digit(s.as_str().chars().rev().collect::<String>().as_str()))
-                .expect("Couldn't find reverse match");
             let num1 = FORWARD_NUM_RE
                 .find(line)
                 .map(|s| parse_digit(s.as_str()))
                 .expect("Couldn't find first digit");
-            digits_to_value(vec![num1, num2])
+            let num2 = BACKWARD_NUM_RE
+                .find(line.chars().rev().collect::<String>().as_str())
+                .map(|s| parse_digit(s.as_str().chars().rev().collect::<String>().as_str()))
+                .expect("Couldn't find reverse match");
+            digits_to_value(num1, num2)
         })
         .sum()
 }
