515aba09dfd34d5ee7f57541004e985197ffcb16
Blaise <blaise@blaise.in>
2023-12-03 22:32:23 -0800

    Cleanup

--- 2023/day04/day04.rs
+++ 2023/day04/day04.rs
@@ -1,5 +1,4 @@
-use std::cmp::max;
-use std::collections::{HashSet, VecDeque};
+use std::collections::HashSet;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -15,23 +14,20 @@ fn parse(lines: &Vec<String>) -> Vec<Game> {
       line
         .split(':')
         .skip(1)
-        .map(|card| {
-          card
-            .split("|")
-            .map(|section| {
-              section
-                .split_whitespace()
-                .map(|x| x.parse::<usize>().expect("Couldn't parse num"))
-                .collect::<HashSet<_>>()
-            })
-            .tuples()
-            .take(1)
-            .exactly_one()
-            .expect("Couldn't split card sections")
-        })
         .take(1)
         .exactly_one()
         .expect("Couldn't parse line")
+        .split("|")
+        .map(|section| {
+          section
+            .split_whitespace()
+            .map(|x| x.parse::<usize>().expect("Couldn't parse num"))
+            .collect::<HashSet<_>>()
+        })
+        .tuples()
+        .take(1)
+        .exactly_one()
+        .expect("Couldn't split card sections")
     })
     .collect()
 }
@@ -45,21 +41,16 @@ fn score_games(games: &Vec<Game>) -> Vec<usize> {
 
 fn part1(games: &Vec<Game>) -> usize {
   score_games(games)
-    .iter()
-    .map(|&num_winning| {
-      if num_winning == 0 {
-        0
-      } else {
-        2usize.pow(max(num_winning as u32, 1) - 1)
-      }
-    })
+    .into_iter()
+    .filter(|&num_winning| num_winning != 0)
+    .map(|num_winning| 2usize.pow(num_winning as u32 - 1))
     .sum::<usize>()
 }
 
 fn part2(games: &Vec<Game>) -> usize {
   let scored = score_games(&games);
   let mut games_to_score: Vec<usize> = (0..games.len()).collect();
-  let mut i = 0usize;
+  let mut i = 0;
   while games_to_score.len() > 0 {
     let game = games_to_score
       .pop()
